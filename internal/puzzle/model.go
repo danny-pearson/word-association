54fa44af407761cb1780e2e44b098209ff1d8386
dannypearson <danny@crtr.co.uk>
2026-07-30 14:19:08 +0100

    Adds new `Puzzle` type to `internal/puzzle`

--- internal/puzzle/model.go
+++ internal/puzzle/model.go
@@ -1,5 +1,11 @@
 package puzzle
 
+type Puzzle struct {
+	ID               int64  `json:"id"`
+	Answer           string `json:"answer"`
+	AnswerNormalized string `json:"answer_normalized"`
+}
+
 type GeneratedPuzzle struct {
 	Answer string   `json:"answer"`
 	Clues  []string `json:"clues"`
